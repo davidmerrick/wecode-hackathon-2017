9d83c7647f546fdbf8d8e0ae1fa965e320d9bc5e
alexissantina <alexissmj@gmail.com>
2017-12-02 13:27:54 -0800

    Adding firebase config to angular component js

--- angularquickstart/src/app/app.component.ts
+++ angularquickstart/src/app/app.component.ts
@@ -9,9 +9,22 @@ export class AppComponent {
   title = 'app';
 }
 
+var firebase = require("firebase");
 var admin = require("firebase-admin");
+require("firebase/auth");
+require("firebase/database");
 
 var serviceAccount = require("servicekey.json");
+
+var config = {
+    apiKey: "AIzaSyAYpF022ZdJdaparUNIiYscYJQExmlDcKo",
+    authDomain: "hello-world-8a89f.firebaseapp.com",
+    databaseURL: "https://hello-world-8a89f.firebaseio.com",
+    projectId: "hello-world-8a89f",
+    storageBucket: "hello-world-8a89f.appspot.com",
+    messagingSenderId: "735912752935"
+};
+firebase.initializeApp(config);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
